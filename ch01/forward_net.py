934b9632dea849560ffc463bee1846c9279fd45a
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2019-02-17 18:41:12 +0900

    add SoftmaxWithLoss layer

--- ch01/forward_net.py
+++ ch01/forward_net.py
@@ -38,6 +38,30 @@ class Affine:
         return out
 
 
+class SoftmaxWithLoss:
+    """
+    Softmax with Lossレイヤ
+    """
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        """
+        順伝播
+        """
+        self.t = t
+        self.y = softmax(x)
+
+        if self.t.size == self.y.size:
+            self.t = self.t.argmax(axis=1)
+
+        loss = cross_entropy_error(self.y, self.t)
+
+        return loss
+
+
 class TwoLayerNet:
     """
     2層ニューラルネットワーク
@@ -45,16 +69,18 @@ class TwoLayerNet:
     def __init__(self, input_size, hidden_size, output_size):
         I, H, O = input_size, hidden_size, output_size
 
-        W1 = np.random.randn(I, H)
-        b1 = np.random.randn(H)
-        W2 = np.random.randn(H, O)
-        b2 = np.random.randn(O)
+        W1 = 0.01 * np.random.randn(I, H)
+        b1 = np.zeros(H)
+        W2 = 0.01 * np.random.randn(H, O)
+        b2 = np.zeros(O)
 
         self.layers = [
             Affine(W1, b1),
             Sigmoid(),
             Affine(W2, b2),
         ]
+
+        self.loss_layer = SoftmaxWithLoss()
 
         self.params = []
 
@@ -70,10 +96,62 @@ class TwoLayerNet:
 
         return x
 
+    def forward(self, x, t):
+        """
+        順伝播
+        """
+        score = self.predict(x)
+        loss = self.loss_layer.forward(score, t)
+
+        return loss
+
+
+def softmax(x):
+    """
+    ソフトマックス
+    """
+    if x.ndim == 2:
+        x = x - x.max(axis=1, keepdims=True)
+        x = np.exp(x)
+        x /= x.sum(axis=1, keepdims=True)
+
+    elif x.ndim == 1:
+        x = x - np.max(x)
+        x = np.exp(x) / np.sum(np.exp(x))
+
+    return x
+
+
+def cross_entropy_error(y, t):
+    """
+    交差エントロピー誤差
+    """
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+
+    batch_size = y.shape[0]
+
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
 
 if __name__ == '__main__':
     x = np.random.randn(10, 2)
     model = TwoLayerNet(2, 4, 3)
     s = model.predict(x)
-
     print(s)
+
+    test = np.array([[i * j for i in range(5)] for j in range(3)])
+    print(test)
+    print(softmax(test))
+
+    y1 = np.array([0.1, 0.2, 0.7])
+    t1 = np.array([0, 0, 1])
+    print(cross_entropy_error(y1, t1))
+
+    y1 = np.array([[0.1, 0.2, 0.7], [0.1, 0.2, 0.7], [0.1, 0.2, 0.7]])
+    t1 = np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]])
+    print(cross_entropy_error(y1, t1))
